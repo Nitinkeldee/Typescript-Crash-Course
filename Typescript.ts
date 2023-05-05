2401ed99575d34abb5e4a63f30dd41cbe9b1f086
Nitin Kelde <129687057+Nitinkeldee@users.noreply.github.com>
2023-05-05 22:45:01 +0530

    Update Typescript.ts

--- Typescript.ts
+++ Typescript.ts
@@ -190,3 +190,33 @@
 // function getOldest<T extends HasAge>(people: T[]): T {
 //     return people.sort((a, b) => b.age - a.age)[0];
 // }
+
+// const people: HasAge[] = [{ age: 30 }, { age: 40 }, { age: 10 }];
+
+// interface Player {
+//     name: string;
+//     age: number;
+// }
+// const players: Player[] = [
+//     { name: 'John', age: 30 },
+//     { name: 'Jane', age: 35 },
+//     { name: 'Joe', age: 16 },
+// ];
+// // Assertion
+// // const person = getOldest(players) as Player;
+
+// const person = getOldest(people);
+// // Generics
+// person.age;
+
+interface IPost {
+    title: string;
+    id: number;
+    description: string;
+}
+
+interface IUser {
+    id: number;
+    name: string;
+    age: number;
+}
